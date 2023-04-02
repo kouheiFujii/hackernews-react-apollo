5a850e72272042b02a4fbee64d7a3883a7fbbd6c
kouheifujii <kohei.fujii@i-nations.tokyo>
2023-04-03 05:21:30 +0900

    preparing the react components.

---
+++ src/components/Link.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Link(props) {
+  const { link } = props;
+  return (
+    <div>
+      <div>
+        {link.description} ({link.url})
+      </div>
+    </div>
+  );
+}
+
+export default Link;
