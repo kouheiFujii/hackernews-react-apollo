ef8186d0fa10a793a909e54395890125f9759e38
kouheifujii <kohei.fujii@i-nations.tokyo>
2023-04-08 12:55:17 +0900

    Add /login path.

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import "./../styles/App.css";
 import CreateLink from "./CreateLink";
 import LinkList from "./LinkList";
 import Header from "./Header";
+import Login from "./Login";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LinkList />} />
           <Route path="/create" element={<CreateLink />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </div>
     </div>
