abffc812945bb4873dc939f33d79a9fa5e8717af
kouheifujii <kohei.fujii@i-nations.tokyo>
2023-04-08 13:20:00 +0900

    Authentication function.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,23 @@
+import { useMutation, gql } from "@apollo/client";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { AUTH_TOKEN } from "../constants";
+
+const SIGNUP_MUTATION = gql`
+  mutation SignupMutation($email: String!, $password: String!, $name: String!) {
+    signup(email: $email, password: $password, name: $name) {
+      token
+    }
+  }
+`;
+
+const LOGIN_MUTATION = gql`
+  mutation LoginMutation($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+    }
+  }
+`;
 
 const Login = () => {
   const navigate = useNavigate();
@@ -10,6 +28,28 @@ const Login = () => {
     name: "",
   });
 
+  const [login] = useMutation(LOGIN_MUTATION, {
+    variables: {
+      email: formState.email,
+      password: formState.password,
+    },
+    onCompleted: ({ login }) => {
+      localStorage.setItem(AUTH_TOKEN, login.token);
+      navigate("/");
+    },
+  });
+
+  const [signup] = useMutation(SIGNUP_MUTATION, {
+    variables: {
+      email: formState.email,
+      password: formState.password,
+      name: formState.name,
+    },
+    onCompleted: ({ signup }) => {
+      localStorage.setItem(AUTH_TOKEN, signup.token);
+      navigate("/");
+    },
+  });
   return (
     <div>
       <h4 className="mv3">{formState.login ? "Login" : "Sign Up"}</h4>
@@ -53,7 +93,7 @@ const Login = () => {
       <div className="flex mt3">
         <button
           className="pointer mr2 button"
-          onClick={() => console.log("onClick")}
+          onClick={formState.login ? login : signup}
         >
           {formState.login ? "login" : "create account"}
         </button>
